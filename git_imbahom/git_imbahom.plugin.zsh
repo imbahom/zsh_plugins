4352c403459da4ec9877a685d8f773d35c218472
imbahom <imbahom@gmail.com>
2015-06-18 18:45:20 +0800

    -

--- git_imbahom/git_imbahom.plugin.zsh
+++ git_imbahom/git_imbahom.plugin.zsh
@@ -1,12 +1,17 @@
 
 function gitlistD(){
-    git status --short | awk '{if($1=="D") print$2}'
+    git status --porcelain | awk '{if($1=="D") print$2}'
 }
 
 function gitlistM(){
-    git status --short | awk '{if($1=="M") print$2}'
+    git status --porcelain | awk '{if($1=="M") print$2}'
 }
 
 function gitlistUntrackedFile(){
-    git status --short | awk '{if($1=="??") print$2}'
+    git status --porcelain | awk '{if($1=="??") print$2}'
+} 
+
+function gitlistModifiedNewFile(){
+    git status --porcelain | awk '{if($1=="AM" print$2}'
 }
+
