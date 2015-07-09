78d64cef06f56d86f3369f90422eb063c62b0452
imbahom <imbahom@gmail.com>
2015-07-09 16:01:41 +0800

    create br

--- git_imbahom/git_imbahom.plugin.zsh
+++ git_imbahom/git_imbahom.plugin.zsh
@@ -1,17 +1,25 @@
 
 function gitlistD(){
-    git status --porcelain | awk '{if($1=="D") print$2}'
+    git status --short | awk '{if($1=="D") print$2}'
 }
 
 function gitlistM(){
-    git status --porcelain | awk '{if($1=="M") print$2}'
+    git status --short | awk '{if($1=="M") print$2}'
 }
 
 function gitlistUntrackedFile(){
-    git status --porcelain | awk '{if($1=="??") print$2}'
+    git status --short | awk '{if($1=="??") print$2}'
 } 
 
 function gitlistModifiedNewFile(){
-    git status --porcelain | awk '{if($1=="AM" print$2}'
+    git status --short | awk '{if($1=="AM" print$2}'
 }
 
+function gitlistRenamed(){
+    git status --short | awk '{if($1=="R" print$2}'
+}
+
+function gitcreatebr(){
+    brName=$1
+    git branch -D $brName ; git branch $brName ; git checkout $brName
+}
