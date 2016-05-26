9930dd8ea02d9ec00e984a8d32b8119bdeda3691
imbahom <imbahom@gmail.com>
2016-05-26 16:38:33 +0800

    simple script :convert HTML to kindle boooook using kindlegen

--- dailyAlias/dailyAlias.plugin.zsh
+++ dailyAlias/dailyAlias.plugin.zsh
@@ -1,5 +1,29 @@
+
 function readLuaSrc(){
     (cd $CODE/lua-5.1.4 && mvim **/*.c */*.h)
+}
+
+function makeKindleBook(){
+    html=$1
+    file=$2
+    pid=$$
+    (mkdir -p /tmp/$pid  && cd /tmp/$pid && wget -p -k $html)
+    
+    for htmlfile in `find /tmp/$pid -type f | xargs file | grep HTML | cut -d : -f 1`; do
+        basefilename=$(basename $htmlfile)
+        suffix=${basefilename##*.}
+        if [[ $suffix == "html" ]]; then
+            # echo 'kindlegen ' $htmlfile
+            kindlegen -verbose $htmlfile
+        elif [[ $suffix == "" ]]; then
+            newfile=$htmlfile".html"
+            cp $htmlfile $newfile
+            # echo 'kindlegen ' $newfile
+            kindlegen -verbose $newfile
+        fi
+    done
+    find /tmp/$pid -name "*.mobi" -exec mv -v {} $KINDLE_BOOKS/mobis \;
+    rm -fr /tmp/$pid
 }
 
 alias tma="tmux attach"
